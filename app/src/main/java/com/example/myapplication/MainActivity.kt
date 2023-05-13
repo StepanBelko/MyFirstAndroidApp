4ec6831faa1e60c715f1807b5e301d06023460fb
Stepan Belko <Stpn.Belko@gmail.com>
2023-05-13 19:25:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.myapplication
+
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        textView = findViewById(R.id.textView)
+    }
+
+    fun toastMe(view: View){
+        val myToast = Toast.makeText(this, "Hello, Toast!", Toast.LENGTH_SHORT)
+
+        myToast.show()
+    }
+
+    fun countMe(view: View){
+        val countString = textView.text.toString()
+
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        textView.text = count.toString()
+    }
+
+    fun randomMe(view: View) {
+        val randomIntent = Intent(this, SecondActivity::class.java)
+        val countString = textView.text.toString()
+        val currentCount: Int = Integer.parseInt(countString)
+        randomIntent.putExtra(SecondActivity.TOTAL_COUNT, currentCount)
+
+        startActivity(randomIntent)
+    }
+}
